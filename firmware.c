b21b97bf4a3d75992c31c612b8343757342ea0c1
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2016-04-08 21:43:19 +0200

    Working LED.

--- firmware.c
+++ firmware.c
@@ -5,20 +5,20 @@
 //
 void InitialiseSystemClock()
 {
-CLK_ICKR = 0;					   //  Reset the Internal Clock Register.
-CLK_ICKR = CLK_ICKR_HSIEN;				 //  Enable the HSI.
-CLK_ECKR = 0;					   //  Disable the external clock.
-while (!(CLK_ICKR & CLK_ICKR_HSIRDY));	   //  Wait for the HSI to be ready for use.
-CLK_CKDIVR = 0;					 //  Ensure the clocks are running at full speed.
-CLK_PCKENR1 = 0xff;				 //  Enable all peripheral clocks.
-CLK_PCKENR2 = 0xff;				 //  Ditto.
-CLK_CCOR = 0;					   //  Turn off CCO.
-CLK_HSITRIMR = 0;				   //  Turn off any HSIU trimming.
-CLK_SWIMCCR = 0;					//  Set SWIM to run at clock / 2.
-CLK_SWR = 0xe1;					 //  Use HSI as the clock source.
-CLK_SWCR = 0;					   //  Reset the clock switch control register.
-CLK_SWCR = CLK_SWCR_SWEN;				  //  Enable switching.
-while (CLK_SWCR & CLK_SWCR_SWBSY);		//  Pause while the clock switch is busy.
+	CLK_ICKR = 0;					   //  Reset the Internal Clock Register.
+	CLK_ICKR = CLK_ICKR_HSIEN;				 //  Enable the HSI.
+	CLK_ECKR = 0;					   //  Disable the external clock.
+	while (!(CLK_ICKR & CLK_ICKR_HSIRDY));	   //  Wait for the HSI to be ready for use.
+	CLK_CKDIVR = 0;					 //  Ensure the clocks are running at full speed.
+	CLK_PCKENR1 = 0xff;				 //  Enable all peripheral clocks.
+	CLK_PCKENR2 = 0xff;				 //  Ditto.
+	CLK_CCOR = 0;					   //  Turn off CCO.
+	CLK_HSITRIMR = 0;				   //  Turn off any HSIU trimming.
+	CLK_SWIMCCR = 0;					//  Set SWIM to run at clock / 2.
+	CLK_SWR = 0xe1;					 //  Use HSI as the clock source.
+	CLK_SWCR = 0;					   //  Reset the clock switch control register.
+	CLK_SWCR = CLK_SWCR_SWEN;				  //  Enable switching.
+	while (CLK_SWCR & CLK_SWCR_SWBSY);		//  Pause while the clock switch is busy.
 }
 
 //
@@ -33,11 +33,11 @@ int main(void)
 	InitialiseUSART();
 	__enable_interrupt();
 	// Configure pins
-	PD_DDR = (1<<3);
-	PD_CR1 = (1<<3);
+	PB_DDR = (1<<5);
+	PB_CR1 = (1<<5);
 	// Loop
 	do {
-		PD_ODR ^= (1<<3);
+		PB_ODR ^= (1<<5);
 		for(d = 0; d < 29000; d++) { }
 		USARTPrintf("Hello from my microcontroller....\n\r");
 	} while(1);
